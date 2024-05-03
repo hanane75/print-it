72f1a84344306ee849196ca9cdb419014ad00df2
SALHI Hanane <benkada.hanane@gmail.com>
2024-05-04 00:00:31 +0200

    changement d'image et du point au clic que la fléche

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,90 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	  image: "slide1.jpg",
+	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	  image: "slide2.jpg",
+	  tagLine:
+		"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	  image: "slide3.jpg",
+	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-
-
-//sélectionner un élément HTML 
-const bannerImg = document.querySelector('.banner-img');
-const arrowLeft = document.querySelector('.arrow_left');
-const arrowRight = document.querySelector('.arrow_right');
-const dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
-let currentIndex = 0;
-
-
-// Gestionnaire d'événement pour le clic sur la flèche gauche
-arrowLeft.addEventListener('click', function () {
-    currentIndex = (currentIndex - 1);
-    
-});
-
-// Gestionnaire d'événement pour le clic sur la flèche droite
-arrowRight.addEventListener('click', function () {
-    currentIndex = (currentIndex + 1) ;
+	  image: "slide4.png",
+	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+	},
+  ];
   
-});
+  let position = 0; 
+  const numberOfSlide = slides.length;
+  
+  createCaroussel(position);
+  createDots();
+  updateDot();
+  
+  //récuperation des élements html
+  const left = document.querySelector('.arrow_left');
+  const right = document.querySelector('.arrow_right');
+  
+  //ajout des event listener sur chacune des flèches.
+
+  left.addEventListener("click", function () {
+	  if (position == 0) {
+		  position = numberOfSlide - 1;
+	  }
+	  else {
+		  position--;
+	  }
+		  createCaroussel(position);
+  });
+  
+  right.addEventListener("click", function () {
+	  if (position == numberOfSlide - 1) {
+		  position = 0;
+	  } else {
+			  position++;
+	  }
+	  createCaroussel(position);
+  
+  });
+  //fonction pour les points 
+  function createDots(){
+	   const dots = document.querySelector(".dots");	
+	  for (let index = 0; index < slides.length; index++) {
+		  // Pour chaque element dans la boucle je vais créer un dot
+		  const dot= document.createElement("div");
+		  dot.setAttribute("class", "dot");
+		  dots.appendChild(dot);
+	  } 
+  }
+  
+  function updateDot() {
+	const listPoints = document.querySelectorAll(".dot");	
+	 for (let index = 0; index < listPoints.length; index++) {
+	 
+	  const dot = listPoints[index];
+	  if (index == position){
+		  dot.classList.add('dot_selected');		
+	}
+	else{
+	  dot.classList.remove('dot_selected');	 
+	}
+	  
+	  }  
+  }
+  
+  function createCaroussel(position){
+	  
+		const element = slides[position];
+		  //console.log(element);
+		  const img = document.querySelector(".banner-img");
+		  img.setAttribute("src" , "./assets/images/slideshow/"+ element.image);
+  
+		  const p = document.querySelector(".banner-txt");
+		  p.innerHTML= element.tagLine;
+  
+	  updateDot();
+  }
